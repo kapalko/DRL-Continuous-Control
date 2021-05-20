6dd3628e2e09d70d165b460f1377208e9315e44b
Kyle Palko <mrgtreeskier@gmail.com>
2021-05-20 14:47:54 -0400

    Added replay buffer to agent.

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -1,4 +1,5 @@
-### Derived from Udacity DDPG workbooks
+### Derived from Udacity DDPG workbooks and "Continuous Control with Deep Reinforcement Learning"
+### https://arxiv.org/pdf/1509.02971.pdf
 
 import numpy as np
 import random
@@ -52,14 +53,30 @@ class Agent():
         self.noise = OU_Noise(action_size, random_seed)
         
         # initialize replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
         
-    def step():
+    def step(self, state, action, reward, next_state, done):
+        
+        # add state and reward to memory
+        self.memory.add(state, action, reward, next_state, done)
+        
+        # if enough samples are available in memory
+        if len(self.memory) > BATCH_SIZE:
+            experiences = self.memory.sample()
+            self.learn(experiences, GAMMA)
     
     def act():
+    
         
     def learn():
+    
         
     def reset():
+    
+        
+    def soft_update():
+
+
 
 class OU_Noise():
     """Ornstein-Uhlenbeck process."""
@@ -81,3 +98,39 @@ class OU_Noise():
         dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
         self.state = x + dx
         return self.state
+    
+
+class ReplayBuffer:
+    """Buffer to store experience tuples."""
+    
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of the buffer
+            batch_size (int): size of each training batch
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.seed = random.seed(seed)
+        self.experience = namedtuple('Experience', field_names=['state', 'action', 'reward', 'next_state', 'done'])
+        
+    def add(self):
+        """Add experience to the memory buffer"""
+        
+        ex = self.experience(state, action, reward, next_state, done)
+        self.memory.append(ex)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences from memory"""
+        experiences = random.sample(self.memory, k=self.batch_size)
+        
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        
+        return (states, actions, rewards, next_states, dones)
+        
--- model.py
+++ model.py
@@ -5,3 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def hidden_init(layer):
+    dim = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(dim)
+    return (-lim, lim)
