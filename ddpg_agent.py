f7a588ca6d6cb48e08706fcca48291621a417948
Kyle Palko <mrgtreeskier@gmail.com>
2021-07-30 14:38:22 -0400

    Made training more efficient

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 512        # minibatch size
+BATCH_SIZE = 1024        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # soft update of target parameters
 LR_ACTOR = 1e-4         # actor learning rate
@@ -55,10 +55,11 @@ class Agent():
         # initialize replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
         
-    def step(self, state, action, reward, next_state, done):
+    def step(self, states, actions, rewards, next_states, dones):
         
         # add state and reward to memory
-        self.memory.add(state, action, reward, next_state, done)
+        for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+            self.memory.add(state, action, reward, next_state, done)
         
         # if enough samples are available in memory
         if len(self.memory) > BATCH_SIZE:
@@ -94,8 +95,8 @@ class Agent():
         
         # -----update the critic-----
         # get predicted next-state actions and Q values from targets
-        actions_next = self.actor_target(next_states)
-        Q_targets_next = self.critic_target(next_states, actions_next)  # maps the (state, action) pairs to the Q-values
+        actions_next = self.actor_target(next_states.to(device))
+        Q_targets_next = self.critic_target(next_states.to(device), actions_next.to(device))  # maps the (state, action) pairs to the Q-values
         # compute Q targets for current states, y_i
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))  # we use 1-dones since there won't be any discounted rewards since the episode ends
         # compute the critic loss
@@ -120,6 +121,8 @@ class Agent():
         # update the target networks
         self.soft_update(self.critic_local, self.critic_target, TAU)
         self.soft_update(self.actor_local, self.actor_target, TAU)
+        
+        self.noise.reset() # reset noise after learning
         
     def reset(self):
         self.noise.reset()
